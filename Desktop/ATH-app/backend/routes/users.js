a2ace21a2f5f5abe923a358a1276df8823c384eb
BouzirJawad <jawadbouzir01@gmail.com>
2025-03-03 10:07:54 +0000

    register in frontend add

--- Desktop/ATH-app/backend/routes/users.js
+++ Desktop/ATH-app/backend/routes/users.js
@@ -6,16 +6,24 @@ const router = express.Router()
 const secretKey = "30b50136e000496c4e0ef82d6b789e6964c77ed757f99ed508844fc18cec8b9a"
 
 router.post('/register', async (req, res) => {
+
+    const { username, email, password } = req.body
+
     try {
-        const newUser = new User({
-            email: req.body.email,
-            password: await bcrybt.hash(req.body.password, 10)
-        })
+        const user = await User.findOne({email})
+        
+        if (user) {
+            return res.status(404).json({ message: "User already exists" })
+        }
+        
+        
+        const hashPass = await bcrybt.hash(password, 10)
+        const newUser = new User({username, email, password: hashPass})
 
         await newUser.save()
         res.status(201).json({message: "User registered successfully"});
     } catch (error) {
-        res.status(500).json({error: 'Error registering user (email already used)'})
+        res.status(500).json({error: 'Error registering user'})
     }
 })
 
@@ -24,12 +32,12 @@ router.post('/login', async (req, res) => {
         const { email, password } = req.body
         const user = await User.findOne({email})
         if (!user) {
-            res.send('User not found')
+            res.status(400).json({ message: "User not found" })
             return
         }
         const isMatched = await bcrybt.compare(password, user.password)
         if (!isMatched) {
-            res.send("Wrong password")
+            res.status(400).json({ message: "Invalid credentials" })
             return
         }
         const token = jwt.sign({ email: user.email }, secretKey, { expiresIn: '1h'})
--- Desktop/ATH-app/frontend/src/components/LoginPage.jsx
+++ Desktop/ATH-app/frontend/src/components/LoginPage.jsx
@@ -1,43 +1,89 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  const [isRegistering, setIsRegistering] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [message, setMessage] = useState('');
-    const navigate = useNavigate()
+  const handleLogin = async () => {
+    try {
+      const path = isRegistering
+        ? "http://localhost:7000/api/users/register"
+        : "http://localhost:7000/api/users/login";
+      const profile = isRegistering
+        ? { username, email, password }
+        : { email, password };
 
-    const handleLogin = async () => {
-        try {
-            const res = await axios.post('http://localhost:7000/api/users/login', { email, password });
-            if (res.data.token) {
-                localStorage.setItem('token', res.data.token);
-                setMessage("Logged In");
+      const res = await axios.post(path, profile);
 
-                setTimeout(() => { navigate('/Csoon')}, 2000);
-            } else {
-                setMessage('Login failed! Check your email and password.');
-            }
-        } catch (error) {
-            setMessage('Login failed!');
-        }
-    };
+      if (res.data.token) {
+        localStorage.setItem("token", res.data.token);
+        setMessage("Logged In ...");
+        setTimeout(() => {
+          navigate("/Csoon");
+        }, 2000);
+      } else {
+        setMessage(
+          res.data.message || "Login failed! Check your email and password."
+        );
+      }
+    } catch (error) {
+      setMessage("Login failed!");
+    }
+  };
 
   return (
     <>
-        <div className="flex flex-col items-center justify-center min-h-screen ">
-            <div className="bg-white p-6 rounded-lg shadow-md">
-                <h2 className="text-2xl text-amber-500 mb-4">Login</h2>
-                <input className="border p-2 mb-2 w-full" type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
-                <input className="border p-2 mb-2 w-full" type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-                <button className="primary-btn w-full" onClick={handleLogin}>Login</button>
-                <p className="text-amber-500 mt-2 text-center">{message}</p>
-            </div>
+      <div className="flex flex-col items-center justify-center min-h-screen ">
+        <div className="bg-white p-6 rounded-lg shadow-md w-[80%]">
+          <h2 className="text-2xl text-amber-500 mb-4">
+            {isRegistering ? "Resiter" : "Login"}
+          </h2>
+          {isRegistering && (
+            <input
+              className="border p-2 mb-2 w-full"
+              type="text"
+              placeholder="Full Name"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          )}
+          <input
+            className="border p-2 mb-2 w-full"
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            className="border p-2 mb-2 w-full"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className="primary-btn w-full" onClick={handleLogin}>
+            {isRegistering ? "Register" : "Login"}
+          </button>
+          <p className="text-amber-500 mt-2 text-center">{message}</p>
+          <p className="mt-4 text-sm text-center">
+            {isRegistering ? "Already have an account?" : "New user?"}{" "}
+            <button
+              className="text-blue-500 underline"
+              onClick={() => setIsRegistering(!isRegistering)}
+            >
+              {isRegistering ? "Login here" : "Register here"}
+            </button>
+          </p>
         </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
